1f54e5db6546ee390f47ff7768524ac3d9aba0b5
Vukasin Nedeljkovic <58425897+VuksaNed@users.noreply.github.com>
2022-07-09 16:31:11 +0200

    Create Balon.java

---
+++ Baloon/Balon.java
@@ -0,0 +1,11 @@
+package baloncici;
+
+import java.awt.Color;
+
+public class Balon extends Kruznafigura {
+
+	public Balon(Vektor centar, Color boja, double precnik, Vektor brzina, Scena scena) {
+		super(centar, boja, precnik, brzina, scena);
+	}
+
+}
